7413fd67a68704f6b5ca553033b17537a3b92538
user <pointgad@gmail.com>
2025-04-26 17:37:23 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,23 @@ import profileImg from './assets/images/zi-a.png';
 function App() {
   return (
     <div className="main-container">
-      <h1 className="title">정지아의 리액트 ✨</h1>
+      <h1 className="title">곽은주의 리액트 ✨</h1>
 
       <ProfileCard
         image={profileImg}
-        name="정지아"
-        description="신소재를 사랑하는 개발자입니다."
+        name="곽은주"
+        description="멍멍이를 사랑하는 개발자입니다."
       />
 
       <div className="link-area">
+
         <a
           className="link-button"
-          href="https://chapter3-react1.vercel.app/"
+          href="https://coffee2-xi.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📘 CHAPTER 3 - 기초
-        </a>
-        <a
-          className="link-button"
-          href="https://chapter3-react2.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          📙 CHAPTER 3 게시판 CRUD
-        </a>
-        <a
-          className="link-button"
-          href="https://chapter3-react3.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          📗 CHAPTER 3 게시판 + MySQL
+          ☕ coffee - 기초
         </a>
       </div>
     </div>
